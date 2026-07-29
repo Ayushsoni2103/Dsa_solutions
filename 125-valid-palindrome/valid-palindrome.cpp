9ceab9f4534e9a36437e502178b59c1fb4776eed
Ayushsoni2103 <ayushsoni9907@gmail.com>
2026-07-29 18:56:54 +0530

    Time: 0 ms (100.00%) | Memory: 9.7 MB (94.48%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.cpp
+++ 125-valid-palindrome/valid-palindrome.cpp
@@ -1,18 +1,20 @@
 class Solution {
 public:
     bool isPalindrome(string s) {
-        string ans;
-        for(char ch:s){
-            if(isalnum(ch)){
-                ans+=tolower(ch);
-            }
-        }
-
-
     int left=0;
-        int right=ans.length()-1;
-        while(left<=right){
-            if(ans[left]!=ans[right]){
+        int right=s.length()-1;
+        while(left<right){
+            char leftChar = tolower(s[left]);
+            char rightChar = tolower(s[right]);
+            if(!isalnum(rightChar)){
+                right--;
+                continue;
+            }
+              if(!isalnum(leftChar)){
+                left++;
+                continue;
+            }
+            if(rightChar!=leftChar){
                 return false;
              
             }
