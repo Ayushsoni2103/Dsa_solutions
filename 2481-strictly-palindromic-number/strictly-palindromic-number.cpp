0c0df9bd17c4c6ea0739490cb145dd537c737904
Ayushsoni2103 <ayushsoni9907@gmail.com>
2026-07-29 19:20:56 +0530

    Time: 0 ms (100.00%) | Memory: 7.6 MB (91.17%) - LeetSync

--- 2481-strictly-palindromic-number/strictly-palindromic-number.cpp
+++ 2481-strictly-palindromic-number/strictly-palindromic-number.cpp
@@ -1,31 +1,7 @@
 class Solution {
 public:
     bool isStrictlyPalindromic(int n) {
-        int b=2;
+        return false;
         
-        while(b<=(n-2)){
-            string a="";
-            int bomb=n;
-            while(bomb>0){
-            a+=bomb%b+'0';
-            bomb=bomb/2;
-            }
-        reverse(a.begin(),a.end());
-            
-    int left=0;
-        int right=a.length()-1;
-        while(left<right){
-            if(a[left]!=a[right]){
-                return false;
-             
-            }
-               left++;
-                right--;
-        }
-        b++;
-          
-
-        }
-        return true;
     }
 };
