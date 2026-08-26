dccfc1b910c988920bf8d36787cf50699d76f281
Ayushsoni2103 <ayushsoni9907@gmail.com>
2026-08-26 12:28:17 +0530

    Time: 88 ms (5.18%) | Memory: 30.5 MB (12.31%) - LeetSync

---
+++ 383-ransom-note/ransom-note.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool canConstruct(string ransomNote, string magazine) {
+        unordered_map<char,int> mp;
+        for(char ch:ransomNote){
+            mp[ch]++;
+        }
+        for(char ch:magazine){
+            if(mp.find(ch)!=mp.end()){
+                mp[ch]--;
+            }
+            if(mp[ch]==0){
+                mp.erase(ch);
+            }
+        }
+        return mp.empty();
+        
+    }
+};
